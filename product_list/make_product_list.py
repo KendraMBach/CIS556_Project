afa071df641fb4ce5a62b64274b22d40846a4a50
CooperStansbury <cstansbu@umich.edu>
2018-11-11 18:59:18 -0500

    minor updates to product list

--- product_list/make_product_list.py
+++ product_list/make_product_list.py
@@ -11,14 +11,11 @@ fake = Faker()
 print()
 new_rows = []
 
-products = ['The Silas Bissell',
-            'The Boudin',
-            'The Lacombe',
-            'The Marx',
-            'The Guevara']
-
-categories = ['wedding',
-            'casual']
+products = [('The Silas Bissell', 'wedding'),
+            ('The Boudin', 'wedding'),
+            ('The Lacombe', 'casual'),
+            ('The Marx', 'wedding'),
+            ('The Guevara', 'casual')]
 
 styles = ['men',
         'women',
@@ -39,33 +36,32 @@ sizes = ['small','medium', 'large']
 
 product_number = 100000
 
-for category in categories:
+for sub in subcategories:
+    price = decimal.Decimal(random.randrange(10000))/100
     for julia in styles:
-        for sub in subcategories:
-            price = decimal.Decimal(random.randrange(10000))/100
-            for color in colors:
-                if color == 'gold':
-                    price = float(price) * 1.4
+        for color in colors:
+            if color == 'gold':
+                price = float(price) * 1.4
 
-                for sml in sizes:
-                    for product in products:
+            for sml in sizes:
+                for product in products:
 
-                        row = {
-                            'product_number':"A" + str(product_number),
-                            'product_name': str(product) + " " + str(color).capitalize() + " " + str(sub).capitalize() ,
-                            'category':category,
-                            'subcategory':sub,
-                            'style': julia,
-                            'color':color,
-                            'size':sml,
-                            'wholesale_price': (random.randrange(3, 7)/10) * float(price),
-                            'number_in_stock':random.randrange(1000),
-                            'number_from_vendor':random.randrange(100),
-                            'backorder_date': fake.date_between(start_date='today', end_date='+90d'),
-                        }
+                    row = {
+                        'product_number':"A" + str(product_number),
+                        'product_name': str(product[0]) + " " + str(color).capitalize() + " " + str(sub).capitalize() ,
+                        'category':product[1],
+                        'subcategory':sub,
+                        'style': julia,
+                        'color':color,
+                        'size':sml,
+                        'wholesale_price': round((random.randrange(3, 7)/10) * float(price), 2),
+                        'number_in_stock':random.randrange(1000),
+                        'number_from_vendor':random.randrange(100),
+                        'backorder_date': fake.date_between(start_date='today', end_date='+90d'),
+                    }
 
-                        new_rows.append(row)
-                        product_number += 1
+                    new_rows.append(row)
+                    product_number += 1
 
 df = pd.DataFrame(new_rows)
 
